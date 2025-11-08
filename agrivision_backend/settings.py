00aa21f47ebd685f611cb805ca00722ae371700c
Abhiram Nidumolu <145238647+AbhiramNidumolu@users.noreply.github.com>
2025-11-08 07:54:19 +0530

    Restrict CORS to specific frontend URL

--- agrivision_backend/settings.py
+++ agrivision_backend/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
 # -----------------------
 # CORS (for React Frontend)
 # -----------------------
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = "https://capstoneagrivision.vercel.app"
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     "content-type",
